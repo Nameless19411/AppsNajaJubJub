9fe727d853d7b6a3272083e7a2960539a0b3aa9e
Nameless_Developer <einsteinkerk@gmail.com>
2018-01-07 21:41:20 +0700

    todo list (can't edit, delete)

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,43 @@
-export default () => <div>Welcome to next.js!</div>
+import React, { Component } from 'react'
+
+class myApp extends Component{
+    state = {
+        firstLine: 'To Do Lists',
+        input: '',
+        todos: [],
+    }
+
+    handleChange = (event) => {
+        if(event.key === 'Enter'){
+            const newTodos = [...this.state.todos,this.state.input]
+            this.setState({todos: newTodos})
+        }
+        else{
+            this.setState({input: event.target.value});
+        }
+    }
+
+    renderTodo = () => {
+        return this.state.todos.map((item, count) => {
+            return <li key={count}>{item}</li>
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.firstLine}</h1>
+                <input
+                value={this.state.input}
+                onChange={this.handleChange}
+                onKeyPress={this.handleChange}
+                />
+                <ol>
+                    {this.renderTodo()}
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default myApp
